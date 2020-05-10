86a66f2bb0b892981f035507ec85dee5db43bf4c
bardiademon <bardiademon@gmail.com>
2020-05-10 16:57:18 +0430

    Connect mongo and create rot posts and schema posts and insert

--- Posts.js
+++ Posts.js
@@ -1,11 +1,33 @@
 const express = require ('express');
 
-const router = express.Router ();
+const Posts = require ('./model/Posts');
+
+let router;
+router = express.Router ();
 
 router.get ("/" , (req , res) =>
 {
     res.send ("test posts 1545");
     res.end ();
+});
+
+router.post ("/new" , (req , res) =>
+{
+    const post = new Posts ({
+        title : req.body.title ,
+        body : req.body.body
+    });
+
+    post.save ().then (data =>
+    {
+        res.send (data);
+        res.end ();
+    }).catch (e =>
+    {
+        res.send (data.message);
+        res.end ();
+    });
+
 });
 
 module.exports = router;
--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 const express = require ('express');
+const bodyParser = require ('body-parser');
+
+require ("dotenv/config")
 
 const routerPosts = require ('./Posts');
 
 let app;
 app = express ();
 
-app.listen (3000);
+app.use (bodyParser.json ());
 
+const mongoose = require ('mongoose');
 app.use ("/posts" , routerPosts);
 
 app.get ("/" , (req , res) =>
@@ -14,3 +18,12 @@ app.get ("/" , (req , res) =>
     res.send ("/index.html");
     res.end ();
 });
+
+mongoose.connect (process.env.db_connection , {
+    useUnifiedTopology : true ,
+    useNewUrlParser : true
+} , () => console.log ("connected!"));
+
+app.listen (3000);
+
+
