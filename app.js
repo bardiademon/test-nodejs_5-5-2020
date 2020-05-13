899c19d5b6a2273be0c38ad2e3855c9d28dca4c4
bardiademon <bardiademon@gmail.com>
2020-05-13 14:29:50 +0430

    Sign for jwt

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
 import engines from "consolidate";
 import path from "path";
-import routerPosts from "./Posts";
 import jwt from "jsonwebtoken";
+import sha256 from 'sha256';
+import Account from "./model/Account";
+import mongoose from "mongoose";
 
 require ("dotenv/config");
+const { isEmpty } = require ("jsprim");
 let app;
 app = express ();
 
@@ -16,10 +19,6 @@ app.use (express.static (path.join (__dirname , 'public' , 'views')));
 
 app.engine ('html' , engines.mustache);
 app.set ('view engine' , 'html');
-
-const mongoose = require ('mongoose');
-app.use ("/posts" , routerPosts);
-
 app.get ("/" , async (req , res) =>
 {
     res.status (200);
@@ -30,15 +29,35 @@ app.get ("/" , async (req , res) =>
     await res.render (path.join (__dirname , 'public' , 'views' , 'index.ejs') , { is_login : isLogin });
 });
 
-app.post ("/login" , (req , res) =>
+app.post ("/login" , async (req , res) =>
 {
+    res.set ("Content-Type" , "text/json");
     if ((req.body.username !== undefined && req.body.password !== undefined) && (req.body.username !== null && req.body.password !== null))
     {
+        const password = sha256 (req.body.password , { asString : true });
 
+        await Account.find ({
+            username : req.body.username ,
+            password
+        } , async (err , info) =>
+        {
+            if (err || isEmpty (info)) res.json ({ result : 'not_found' });
+            else
+            {
+                await jwt.sign ({ info } , "sec_key" , {} , (err , token) =>
+                {
+                    if (err) res.json ({ result : err.message });
+                    else res.json ({ result : token });
+                });
+            }
+            res.end ();
+        });
     }
-    else res.json ({ result : 'request is null' });
-
-    res.end ();
+    else
+    {
+        res.json ({ result : 'request is null' });
+        res.end ();
+    }
 });
 
 mongoose.connect (process.env.connection , {
